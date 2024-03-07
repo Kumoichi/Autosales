3f176091a6932eaec1dbfddd7298928169f7ca5d
Kumoichi <minimi_ken1@yahoo.co.jp>
2024-03-07 14:03:24 -0800

    pagemovement button

--- resources/views/layout/selection.blade.php
+++ resources/views/layout/selection.blade.php
@@ -9,35 +9,36 @@
 </head>
 
 <body>
+
     <div class="three-container">
     <h2>メール配信指示設定</h2>
 
     </div>
-</div>
-    <div class="three-container">
-        <div class="shape-container">
-            <div class="square">
-                <div class="instruction">配信条件指示</div>
-                <div class="triangle-hide"></div> 
-            </div>
-            <div class="triangle"></div>
-        </div>
 
-        <div class="shape-container">
-            <div class="square">
-            <div class="instruction">配信内容確認</div>
-                <div class="triangle-hide"></div> 
+        <div class="three-container">
+            <div class="shape-container">
+                <div class="square">
+                    <div class="instruction">配信条件指示</div>
+                    <div class="triangle-hide"></div> 
+                </div>
+                <div class="triangle"></div>
             </div>
-            <div class="triangle"></div>
-        </div>
 
-        <div class="shape-container">
-            <div class="square">
-            <div class="instruction3">配信タイミング指定</div>
-                <div class="triangle-hide"></div> 
+            <div class="shape-container">
+                <div class="square">
+                <div class="instruction">配信内容確認</div>
+                    <div class="triangle-hide"></div> 
+                </div>
+                <div class="triangle"></div>
             </div>
-            <div class="triangle"></div>
+
+            <div class="shape-container">
+                <div class="square">
+                <div class="instruction3">配信タイミング指定</div>
+                    <div class="triangle-hide"></div> 
+                </div>
+                <div class="triangle"></div>
+            </div>
         </div>
-    </div>
 </body>
 </html>
--- resources/views/pages/timeselection.blade.php
+++ resources/views/pages/timeselection.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/timeselection.css') }}" rel="stylesheet">
 
+
     <title>Document</title>
 </head>
 <body class="timeselection">
@@ -17,6 +18,17 @@
 
     <p class="underwords">必須項目は（<span style = "color: red;">＊</span>）すべて選択してください</p>
     @include('calendar_form')
+
+    <div class="center-container">
+    <div class="button-wrapper">
+        <a href="targetselection" class="pagemovement-button">前のページ</a>
+        <a href="contentselection" class="pagemovement-button">案件コードの入力</a>
+    </div>
+    <a href="targetselection" class="firstpage-button">最初から設定</a>
+</div>
+
+
+
 </div>
 </body>
 </html>
