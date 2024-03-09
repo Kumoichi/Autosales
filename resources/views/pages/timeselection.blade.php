10ab5f4fca8f9c6a95d67ecbc6fd137c3539ef48
Kumoichi <minimi_ken1@yahoo.co.jp>
2024-03-08 23:02:00 -0800

    modal with anken

--- resources/views/pages/timeselection.blade.php
+++ resources/views/pages/timeselection.blade.php
@@ -23,25 +23,60 @@
         </div>
         <a href="targetselection" class="firstpage-button">最初から設定</a>
     </div>
-</div>
-
 <!-- Modal -->
 <div id="myModal" class="modal">
     <div class="modal-content">
         <span class="close">&times;</span>
-        <p>入力</p>
+        <p id="selectedDateTime"></p>
+        <p id="selectedFrequency"></p>
+        
+
+        <div style="text-align: center;">
+            <p style="display:inline; color:gray; margin-right:20px;">案件コード</p>
+            <input type="text" id="anken_box" name="selected_anken" class="styled-input">
+        </div>
+
+
+        <div style="margin-top:20px; text-align: center; display: flex; justify-content: space-around;">
+            <button class="back-button"id="backButton">戻る</button>
+            <button class="confirm-button" id="confirmButton">確認</button>
+        </div>
     </div>
 </div>
 
+
+<div id="myModal" class="modal">
+    <div class="modal-content">
+        
+    </div>
+</div>
+
+
 <script>
+
+var ankenBox = document.getElementById('anken_box');
+var confirmButton = document.getElementById('confirmButton');
+
+
+    ankenBox.addEventListener('input', function() {
+
+        if (ankenBox.value.trim() !== '') {
+            confirmButton.style.backgroundColor = '#11A7B7'; 
+        } else {
+            confirmButton.style.backgroundColor = 'gray'; 
+        }
+    });
+
+
     var modal = document.getElementById('myModal');
-
     var link = document.getElementById("openModal");
-
     var span = document.getElementsByClassName("close")[0];
+    var modalForm = document.getElementById('modalForm');
+    var confirmButton = document.getElementById('confirmButton');
 
     link.onclick = function () {
         modal.style.display = "block";
+        confirmButton.style.display = "block";
     }
 
     span.onclick = function () {
@@ -53,7 +88,24 @@
             modal.style.display = "none";
         }
     }
-</script>
 
-</body>
-</html>
+    //いったん保留
+    confirmButton.onclick = function () {
+        var selectedDate = document.getElementById('selected_date_with_day').value;
+        var selectedTime = document.getElementById('selected_time').value;
+        var selectedFrequency = document.getElementById('selected_frequency').value;
+
+        document.getElementById('selected_date_modal').value = selectedDate;
+        document.getElementById('selected_time_modal').value = selectedTime;
+        document.getElementById('selected_frequency_modal').value = selectedFrequency;
+
+        document.getElementById('selectedDateTime').textContent = "Date: " + selectedDate + ", Time: " + selectedTime;
+        document.getElementById('selectedFrequency').textContent = "Frequency: " + selectedFrequency;
+
+        modal.style.display = "block";
+    }
+
+    modalForm.addEventListener('submit', function (event) {
+        modalForm.submit();
+    });
+</script>
